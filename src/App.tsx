a5437dfc431a74e78e3d0f050f1cbe25e518aff7
jtsang02 <jtsang02@student.ubc.ca>
2021-11-14 18:23:05 -0800

    UI build

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./style/style.css";
+import Button from "./components/Button";
+import Header from "./components/Header";
+import ToggleButton from "./components/ToggleButton";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="container">
+        
+        <ToggleButton/>
+
+        <div className="btn-themes">
+          <Button
+            color={"#889200"}
+            text={"Theme 1"}
+          //onclick={}      // function call to set theme 1
+          />
+          <Button
+            color={"#76b08e"}
+            text={"Theme 2"}
+          //onclick={}      // function call to set theme 2
+          />
+          <Button
+            color={"gray"}
+            text={"Theme 3"}
+          //onclick={}      // function call to set theme 3
+          />
+          <Button
+            color={"purple"}
+            text={"Theme 4"}
+          //onclick={}      // function call to set theme 4
+          />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Button.js
@@ -0,0 +1,26 @@
+import propTypes from "prop-types";
+
+const button = ({ color, text, onClick }) => {
+
+  return (
+    <button
+      onClick={onClick}
+      style={{ backgroundColor: color }}
+      className="btn"
+    >
+      {text}
+    </button>
+  );
+};
+
+button.defaultProps = {
+  color: "blue",
+};
+
+button.propTypes = {
+  text: propTypes.string,
+  color: propTypes.string,
+  onClick: propTypes.func,
+};
+
+export default button;
